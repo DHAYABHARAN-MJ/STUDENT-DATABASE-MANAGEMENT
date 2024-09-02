56fcd57ad1d00b82c4ade69a6e11a3bea4efcf60
bharani2624 <bharani2624baru@gmail.com>
2024-09-02 14:52:23 +0530

    Converted Dashboard To React

--- react-student-app/src/App.jsx
+++ react-student-app/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+// import { Route,Routes,useLocation,Router } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Login from '../src/Login/Login'; // Adjust the path as necessary
-
+import Dashboard from './Dashboard/Dashboard';
 const App = () => {
+  // const location=useLocation();
+  
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Login /> */}
+        <Routes>
+          <Route path='/' element={<Login />}/>
+          <Route path='/Dashboard' element={<Dashboard />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- react-student-app/src/Login/Login.jsx
+++ react-student-app/src/Login/Login.jsx
@@ -1,19 +1,25 @@
 // Login.jsx
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import googleimg from '../assets/images/google.png';
+import { useNavigate } from 'react-router-dom';
+// import { motion } from 'framer-motion';
+// import googleimg from '../assets/images/google.png';
 import bitsathyimg from '../assets/images/Bannari_Amman_Institute_of_Technology_logo.png';
 import '../Login/Login.css';
+import Dashboard from '../Dashboard/Dashboard';
 
 const CLIENT_ID = '158651131098-2607903rinkb0ubd39nslv4264of44rl.apps.googleusercontent.com';
 
 const Login = () => {
-
+  const history=useNavigate();
   const onSignInSuccess = (credentialResponse) => {
     console.log('ID Token:', credentialResponse.credential);
-    window.location.href = '/dashboard.html';
+    // window.location.href = '/dashboard.html';
   };
-
+  const onsign=()=>
+    {
+      history('/Dashboard');
+    }
   const onSignInFailure = (error) => {
     console.error('Google Sign-In failed:', error);
     alert('Sign-In failed. Please try again.');
@@ -33,13 +39,13 @@ const Login = () => {
             <p className="signin-instruction">
               Sign in with your <span><b>Bitsathy Gmail</b></span>
             </p>
-            <div className="gb">
-              <GoogleLogin
+            <div className="gb" onClick={onsign}>
+              {/* <GoogleLogin className="gsign"
                 onSuccess={onSignInSuccess}
                 onError={onSignInFailure}
-              />
-              <img src={googleimg} width={25} height={25} alt="Google Logo"/>
-              <div className="signin">Sign in</div>
+              /> */}
+              {/* <img src={googleimg} width={25} height={25} alt="Google Logo"/> */}
+              <div className="signin">Google Sign In</div>
             </div>
           </div>
         </div>
