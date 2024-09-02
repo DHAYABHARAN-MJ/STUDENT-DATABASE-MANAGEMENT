effe41c79b2817f8581c93e6bef37b4bb1b4dbb6
bharani2624 <bharani2624baru@gmail.com>
2024-09-02 13:25:44 +0530

    Converted To React

--- react-student-app/src/App.jsx
+++ react-student-app/src/App.jsx
@@ -1,2 +1,12 @@
-import React from "react";
+import React from 'react';
+import Login from '../src/Login/Logina'; // Adjust the path as necessary
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
 export default App;
--- react-student-app/src/Login/Login.jsx
+++ react-student-app/src/Login/Login.jsx
@@ -1,27 +1,51 @@
-import React from "react";
-import googleimg from '../assets/images/google.png'
-import bitsathyimg from '../assets/images/Bannari_Amman_Institute_of_Technology_logo.png'
-import '../Login/Login.css'
-const Login=()=>{
-return(
-    <div className="container">
-        <div class="wb">
-        <center><span id="w">Welcome Back!</span></center>
+// Login.jsx
+import React from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import googleimg from '../assets/images/google.png';
+import bitsathyimg from '../assets/images/Bannari_Amman_Institute_of_Technology_logo.png';
+import '../Login/Login.css';
+
+const CLIENT_ID = '158651131098-2607903rinkb0ubd39nslv4264of44rl.apps.googleusercontent.com';
+
+const Login = () => {
+
+  const onSignInSuccess = (credentialResponse) => {
+    console.log('ID Token:', credentialResponse.credential);
+    window.location.href = '/dashboard.html';
+  };
+
+  const onSignInFailure = (error) => {
+    console.error('Google Sign-In failed:', error);
+    alert('Sign-In failed. Please try again.');
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <div className="container">
+        <div className="wb">
+          <center><span id="w">Welcome Back!</span></center>
         </div>
-        <div class="login-container">
-        <div class="google-signin-container">
-            <div class="bitsathyimg">
-                <img src={bitsathyimg} alt=""/>
+        <div className="login-container">
+          <div className="google-signin-container">
+            <div className="bitsathyimg">
+              <img src={bitsathyimg} alt="Bitsathy Logo" />
             </div>
-            <p class="signin-instruction">Sign in with your <span><b>Bitsathy Gmail</b></span></p>
-            <div class="gb" onclick="handleSignInClick()">
-                <img src={googleimg} width={25} height={25}/>
-                <div class="sigin">Sign in</div>
-                {/* <!-- <center><div class="g-signin2" id="googleSignInBtn"></div></center> --> */}
+            <p className="signin-instruction">
+              Sign in with your <span><b>Bitsathy Gmail</b></span>
+            </p>
+            <div className="gb">
+              <GoogleLogin
+                onSuccess={onSignInSuccess}
+                onError={onSignInFailure}
+              />
+              <img src={googleimg} width={25} height={25} alt="Google Logo"/>
+              <div className="signin">Sign in</div>
             </div>
+          </div>
         </div>
-        </div>
-    </div>
-)
-}
-export default Login
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default Login;
